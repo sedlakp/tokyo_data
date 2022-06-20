d12d8e48665ceb86d60230f9a198a415bd58f459
sedlakp <37884964+sedlakp@users.noreply.github.com>
2022-06-20 20:21:46 +0900

    Add random site to the explore tab

--- lib/ui/category_screen/dashboard_screen.dart
+++ lib/ui/category_screen/dashboard_screen.dart
@@ -1,7 +1,13 @@
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 import 'package:tokyo_data/models/models.dart';
+import 'package:tokyo_data/ui/sites_screen/site_cell.dart';
 import 'category_list_view.dart';
+import '../screens.dart';
 
 
 class DashboardScreen extends StatefulWidget {
@@ -12,24 +18,104 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
+
+  final Random _random = Random();
+
+  int _randomInt = -1;
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
-      child: GridView.builder(
-          itemCount: SiteCategory.values.length,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-          itemBuilder: (context, index) {
-            var siteCategory = SiteCategory.values[index];
+    return Consumer<SitesManager>(builder: (context, manager, child) {
+      if (_randomInt == -1) {
+        _randomInt = _random.nextInt(manager.sites.length);
+      }
+      return Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+            children: [
+              randomSite(manager),
+              categoryGrid(),
+            ]),
+      );
+    },);
+  }
 
-            return GestureDetector(
-                onTap: () {
-                  _itemTapped(siteCategory);
-                },
-                child: Card(child: Center(child: Text(siteCategory.name, style: Theme.of(context).textTheme.headlineMedium,),))
-            );
-          }),
+  Widget randomSite(SitesManager manager) {
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("Random site", style: GoogleFonts.montserrat(fontSize: 20, fontWeight: FontWeight.w500)),
+              IconButton(onPressed: refreshPressed, icon: const Icon(Icons.refresh),splashRadius: 20,),
+            ],
+          ),
+          GestureDetector(
+            onTap: () {
+              pushToDetail(context,manager.sites[_randomInt]);
+            },
+              child: SiteCell(site: manager.sites[_randomInt]
+              )),
+        ]
     );
+  }
+
+  Widget categoryGrid() {
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 16,),
+          Text("Categories", style: GoogleFonts.montserrat(fontSize: 20, fontWeight: FontWeight.w500)),
+          GridView.builder(
+            primary: false,
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: SiteCategory.values.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+              itemBuilder: (context, index) {
+                var siteCategory = SiteCategory.values[index];
+
+                return GestureDetector(
+                    onTap: () {
+                      _itemTapped(siteCategory);
+                    },
+                    child: Card(
+                      clipBehavior: Clip.hardEdge,
+                        child: Container(
+                          foregroundDecoration: const BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Colors.black26,
+                                Colors.transparent,
+                              ],
+                              begin: Alignment.bottomCenter,
+                              end: Alignment.topCenter,
+                              stops: [0, 0.5],
+                            ),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Stack(
+                              children: [
+                                Positioned(
+                                  bottom: 5,
+                                  child: Text(
+                                    maxLines: 3,
+                                    siteCategory.name,
+                                    style: GoogleFonts.montserrat(fontSize: 16),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                    )
+                );
+              }),
+        ],
+      );
   }
 
 
@@ -37,6 +123,20 @@ class _DashboardScreenState extends State<DashboardScreen> {
     Navigator.push(context,
         MaterialPageRoute(builder: (context) {
           return CategoryListView(category: category,);
+        },)
+    );
+  }
+
+  void refreshPressed() {
+    setState(() {
+      _randomInt = -1;
+    });
+  }
+
+  void pushToDetail(BuildContext context, CulturalSite site) {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) {
+          return DetailSiteView(site: site,);
         },)
     );
   }
