b727afca4729252b8ac8ec5b6b9a0e03f0ab34e8
sedlakp <37884964+sedlakp@users.noreply.github.com>
2022-06-20 19:10:53 +0900

    Make pie chart not aspect ratio sized

--- lib/ui/stats_screen/tokyo_pie_graph.dart
+++ lib/ui/stats_screen/tokyo_pie_graph.dart
@@ -25,49 +25,47 @@ class PieChart2State extends State {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 0.9,
+    return SizedBox(
+      height: 440,
       child: Card(
-        color: Colors.white,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: <Widget>[
-              Text("Site Categories",style: Theme.of(context).textTheme.headlineLarge,),
-              const SizedBox(height: 20,),
-              Expanded(
-                child: AspectRatio(
-                  aspectRatio: 1,
-                  child: PieChart(
-                    PieChartData(
-                        pieTouchData: PieTouchData(touchCallback:
-                            (FlTouchEvent event, pieTouchResponse) {
-                          setState(() {
-                            if (!event.isInterestedForInteractions ||
-                                pieTouchResponse == null ||
-                                pieTouchResponse.touchedSection == null) {
-                              touchedIndex = -1;
-                              return;
-                            }
-                            touchedIndex = pieTouchResponse
-                                .touchedSection!.touchedSectionIndex;
-                          });
-                        }),
-                        borderData: FlBorderData(
-                          show: false,
-                        ),
-                        sectionsSpace: 0,
-                        centerSpaceRadius: 20,
-                        sections: showingSections()),
-                  ),
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: <Widget>[
+                Text("Site Categories",style: Theme.of(context).textTheme.headlineLarge,),
+                const SizedBox(height: 20,),
+                Expanded(
+                    child: PieChart(
+                      PieChartData(
+                          pieTouchData: PieTouchData(touchCallback:
+                              (FlTouchEvent event, pieTouchResponse) {
+                            setState(() {
+                              if (!event.isInterestedForInteractions ||
+                                  pieTouchResponse == null ||
+                                  pieTouchResponse.touchedSection == null) {
+                                touchedIndex = -1;
+                                return;
+                              }
+                              touchedIndex = pieTouchResponse
+                                  .touchedSection!.touchedSectionIndex;
+                            });
+                          }),
+                          borderData: FlBorderData(
+                            show: false,
+                          ),
+                          sectionsSpace: 0,
+                          centerSpaceRadius: 20,
+                          sections: showingSections()),
+                    ),
+
                 ),
-              ),
-              const SizedBox(height: 20,),
-              getLegend(),
-            ],
+                const SizedBox(height: 20,),
+                getLegend(),
+              ],
+            ),
           ),
         ),
-      ),
     );
   }
 
